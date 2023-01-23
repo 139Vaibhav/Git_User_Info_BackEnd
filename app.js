ee009575c026ca5474a54e4674e6bc3d2b94214a
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2023-01-24 03:45:15 +0530

    Solved CORs error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+const cors = require("cors");
 
 app.use(express.json())
 
@@ -8,5 +9,8 @@ app.use(express.json())
 const userInfo = require("./routes/userRoute");
 
 app.use("/api/v1", userInfo)
+app.use(cors({
+    origin: "https://bejewelled-empanada-63b3c2.netlify.app"
+}))
 
 module.exports = app
